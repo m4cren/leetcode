71bf3b91f9904db470c94d0ab4028d2310d342d5
m4cren <renrenofficial@gmail.com>
2025-07-27 12:58:55 +0800

    Solved roman to int in python and typescript

---
+++ typescript/roman.ts
@@ -0,0 +1,27 @@
+const romanToInt = (s: string): number=>{
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    let value = 0
+
+    for(let i = 0; i < s.length; i++){
+        if(i < s.length && romanMap[s[i]] < romanMap[s[i+1]]){
+            value -= romanMap[s[i]]
+        }else{
+            value += romanMap[s[i]]
+        }
+    } 
+
+    return value
+}
+
+
+console.log(
+romanToInt('XIV'))
